e301b9a3ff85aa8454dc0ecb8f248bccf76e27fc
RafaelMartins4 <rlo.martins@campus.fct.unl.pt>
2023-10-06 18:00:23 +0100

    Added Rental Class data

--- src/scc/data/Rental.java
+++ src/scc/data/Rental.java
@@ -1,8 +1,68 @@
 package scc.data;
 
+import java.time.LocalDate;
+
 /**
  * Represents a House Rental, as returned to the client
  */
 
 public class Rental {
+    private int id;
+    private House rental;
+    private User renter;
+    private double price;
+    private LocalDate initialDate;
+    private LocalDate endDate;
+
+    public Rental() {
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setRental(House rental) {
+        this.rental = rental;
+    }
+
+    public House rental() {
+        return rental;
+    }
+
+    public void setRenter(User renter) {
+        this.renter = renter;
+    }
+
+    public User renter() {
+        return renter;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setInitialDate(LocalDate initialDate) {
+        this.initialDate = initialDate;
+    }
+
+    public LocalDate getInitialDate() {
+        return initialDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
 }
