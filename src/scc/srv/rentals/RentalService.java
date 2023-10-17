e0752beafcea33d3763c3e691a48bab987499ccb
RafaelMartins4 <113689206+RafaelMartins4@users.noreply.github.com>
2023-10-17 10:31:37 +0100

    Finished RentalService, RentalResource and updated Rental class information

--- src/scc/srv/rentals/RentalService.java
+++ src/scc/srv/rentals/RentalService.java
@@ -1,4 +1,36 @@
 package scc.srv.rentals;
 
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Link;
+import jakarta.ws.rs.core.MediaType;
+import scc.data.*;
+
+import javax.print.attribute.standard.Media;
+import java.util.List;
+
+@Path(RentalService.PATH)
+
 public interface RentalService {
+    String PATH = "/house";
+    String RENTAL_ID = "id";
+    String RENTAL = "/rental";
+
+
+    @POST
+    @Path(RENTAL)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    String createRental(RentalDAO rentalDAO) throws Exception;
+
+    @GET
+    @Path("{" + RENTAL_ID + "}" + RENTAL)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Rental getRental(@PathParam(RENTAL_ID) String id) throws Exception;
+
+    @PUT
+    @Path("/{" + RENTAL_ID + "}" + RENTAL)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    Rental updateRental(@PathParam(RENTAL_ID) String id, RentalDAO rentalDAO) throws Exception;
 }
