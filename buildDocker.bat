8ede9f9ed061648ed5aa5126c9c07f0d81ddbfa0
RafaelCMira <113689920+RafaelCMira@users.noreply.github.com>
2023-12-03 23:11:36 +0000

    Renamed and minor changes to deployKubernetes.ps1 script. Moved project 1 scripts to other folder

--- buildDocker.bat
+++
@@ -1,4 +0,0 @@
-call mvn clean compile package
-:: You can change the name following the -t flag to give a different name to your image
-call docker build -t rlpereirafct2002/scc60700-app ./docker
-call docker push rlpereirafct2002/scc60700-app
