d5f5b19a14a42b491dc238d64ea51df2c149b4c1
RafaelCMira <113689920+RafaelCMira@users.noreply.github.com>
2023-12-04 17:17:43 +0000

    Improved script, now just need to say your student number

--- deployKubernetes.ps1
+++ deployKubernetes.ps1
@@ -1,59 +1,50 @@
-# Prompt the user for the imageName
-$imageName = Read-Host "Enter the Docker image name"
-
-# Check if $imageName is provided
-if (-not $imageName) {
-    Write-Host "Error: Please provide a valid imageName."
-    exit 1
+if ($args.Length -eq 0) {
+    $studentNumber = Read-Host "Enter student number"
+} else {
+    $studentNumber = $args[0]
 }
 
-# Prompt the user for the studentNumber
-$studentNumber = Read-Host "Enter the student number"
+$propertiesContent = (Get-Content -Path ".\config.properties" -Raw)
 
-# Check if $studentNumber is provided
-if (-not $studentNumber) {
-    Write-Host "Error: Please provide a valid student number."
-    exit 1
+if (-not ($propertiesContent -match "^$studentNumber=")) {
+    Write-Host "StudentNumber '$studentNumber' does not exist"
+    Exit 1
 }
 
- # Maven commands
+$imageName = ($propertiesContent | ConvertFrom-StringData)[$studentNumber]
+
 Write-Host "Executing Maven commands..."
 mvn clean compile package
 
-# Docker login
 Write-Host "Logging into Docker..."
 docker login
 
-# Docker build
 Write-Host "Building Docker image..."
 docker build -t $imageName ./docker
 
-# Docker push
 Write-Host "Pushing Docker image..."
 docker push $imageName
 
 # -------------------------------------------- #
 
-# Docker push
 Write-Host "Starting Kubernetes..."
 
-Write-Host "Creating Azure Resource Group..."
-$resourceGroupOutput = az group create --name scc2324-cluster-$studentNumber --location westeurope
+$resourceGroupName = "scc2324-cluster-$studentNumber"
+$clusterName = "my-scc2324-cluster-$studentNumber"
 
+Write-Host "Creating Azure Resource Group..."
+$resourceGroupOutput = az group create --name $resourceGroupName --location westeurope
 $resourceGroupId = ($resourceGroupOutput | ConvertFrom-Json).id
 
-Write-Host "Creating Service... resourceGroupId = $resourceGroupId"
+Write-Host "Creating Service Principal..."
 $serviceOutput = az ad sp create-for-rbac --name http://scc2324-kuber-$studentNumber --role Contributor --scope $resourceGroupId
-
 $serviceJSON = $serviceOutput | ConvertFrom-Json
-$appId = $serviceJSON.appId
-$password = $serviceJSON.password
 
 Write-Host "Creating Cluster..."
-$resourceGroupOutput = az aks create --resource-group scc2324-cluster-$studentNumber --name my-scc2324-cluster-$studentNumber --node-vm-size Standard_B2s --generate-ssh-keys --node-count 2 --service-principal $appId  --client-secret $password
+$resourceGroupOutput = az aks create --resource-group $resourceGroupName --name $clusterName --node-vm-size Standard_B2s --generate-ssh-keys --node-count 2 --service-principal $serviceJSON.appId  --client-secret $serviceJSON.password
 
 Write-Host "Getting Kubernetes credentials..."
-az aks get-credentials --resource-group scc2324-cluster-$studentNumber --name my-scc2324-cluster-$studentNumber
+az aks get-credentials --resource-group scc2324-cluster-$studentNumber --name $clusterName
 
 Write-Host "Applying yaml in docker directory..."
 kubectl apply -f .\docker\sccapp.yaml
